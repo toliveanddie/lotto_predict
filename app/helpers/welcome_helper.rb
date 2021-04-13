69a511ca480e397ba1dba96bc288bc4fe02ee443
norm <toliveanddie76@gmail.com>
2021-04-13 14:42:43 -0400

    fix things

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -555,250 +555,252 @@ end
 ##########################################
 ##########################################
 
-def treasurehunt
+  def treasurehunt
 
-  # get the source
-  doc = Nokogiri::HTML(URI.open('https://lottery.com/previous-results/pa/treasurehuntpa/2021/'))
-  results = []
-  doc.css('td').each do |data|
-    results.push(data.content)
-  end
-
-  latest_draws = Array.new #holds strings of numbers
-
-  #push numbers into array and only get enough so that all numbers are accounted for
-  counter = 3
-  100.times do
-    latest_draws.push(results[counter].split('-'))
-    counter = counter + 5
-    break if latest_draws.flatten.uniq.length == 30
-  end
-
-  #put all 30 numbers into an array as strings
-  all_numbers = Array.new
-  30.times do |t|
-    all_numbers.push((t+1).to_s)
-  end
-
-  #create hash with all numbers and set value to zero
-  candidates = Hash.new
-  all_numbers.each do |a|
-    candidates[a] = 0
-  end
-
-  #set all numbers that were drawn to 1
-  3.times do |draw|
-    latest_draws[draw].each do |pick|
-      candidates[pick] = 1
+    # get the source
+    doc = Nokogiri::HTML(URI.open('https://lottery.com/previous-results/pa/treasurehuntpa/2021/'))
+    results = []
+    doc.css('td').each do |data|
+      results.push(data.content)
     end
-  end
-  
-  #remove all numbers that were drawn
-  candidates.delete_if {|k, v| v == 1}
-  
-  #initialize ranges
 
-  ball_five = Hash.new
-  all_numbers.pop(4).each do |b|
-    candidates.each do |key, value|
-      if key == b
-        ball_five[key] = 0
+    latest_draws = Array.new #holds strings of numbers
+
+    #push numbers into array and only get enough so that all numbers are accounted for
+    counter = 3
+    100.times do
+      latest_draws.push(results[counter].split('-'))
+      counter = counter + 5
+      break if latest_draws.flatten.uniq.length == 30
+    end
+
+    #put all 30 numbers into an array as strings
+    all_numbers = Array.new
+    30.times do |t|
+      all_numbers.push((t+1).to_s)
+    end
+
+    #create hash with all numbers and set value to zero
+    candidates = Hash.new
+    all_numbers.each do |a|
+      candidates[a] = 0
+    end
+
+    #set all numbers that were drawn to 1
+    3.times do |draw|
+      latest_draws[draw].each do |pick|
+        candidates[pick] = 1
       end
     end
-  end
+    
+    #remove all numbers that were drawn
+    candidates.delete_if {|k, v| v == 1}
+    
+    #initialize ranges
 
-  ball_four = Hash.new
-  all_numbers.pop(7).each do |c|
-    candidates.each do |key, value|
-      if key == c
-        ball_four[key] = 0
+    ball_five = Hash.new
+    all_numbers.pop(4).each do |b|
+      candidates.each do |key, value|
+        if key == b
+          ball_five[key] = 0
+        end
       end
     end
-  end
 
-  ball_three = Hash.new
-  all_numbers.pop(8).each do |d|
-    candidates.each do |key, value|
-      if key == d
-        ball_three[key] = 0
+    ball_four = Hash.new
+    all_numbers.pop(7).each do |c|
+      candidates.each do |key, value|
+        if key == c
+          ball_four[key] = 0
+        end
       end
     end
-  end
 
-  ball_two = Hash.new
-  all_numbers.pop(7).each do |e|
-    candidates.each do |key, value|
-      if key == e
-        ball_two[key] = 0
+    ball_three = Hash.new
+    all_numbers.pop(8).each do |d|
+      candidates.each do |key, value|
+        if key == d
+          ball_three[key] = 0
+        end
       end
     end
-  end
 
-  ball_one = Hash.new
-  all_numbers.each do |f|
-    candidates.each do |key, value|
-      if key == f
-        ball_one[key] = 0
+    ball_two = Hash.new
+    all_numbers.pop(7).each do |e|
+      candidates.each do |key, value|
+        if key == e
+          ball_two[key] = 0
+        end
       end
     end
-  end
 
-  # get counts for each number drawn and fill in ranges
-  candidates.each do |key, value|
-    latest_draws.each do |draw|
-      draw.each do |j|
-        if key == j
-          if key.to_i < 5
-            ball_one[key] = ball_one[key] + 1
-          elsif key.to_i < 12
-            ball_two[key] = ball_two[key] + 1
-          elsif key.to_i < 20
-            ball_three[key] = ball_three[key] + 1
-          elsif key.to_i < 27
-            ball_four[key] = ball_four[key] + 1
-          else
-            ball_five[key] = ball_five[key] + 1
+    ball_one = Hash.new
+    all_numbers.each do |f|
+      candidates.each do |key, value|
+        if key == f
+          ball_one[key] = 0
+        end
+      end
+    end
+
+    # get counts for each number drawn and fill in ranges
+    candidates.each do |key, value|
+      latest_draws.each do |draw|
+        draw.each do |j|
+          if key == j
+            if key.to_i < 5
+              ball_one[key] = ball_one[key] + 1
+            elsif key.to_i < 12
+              ball_two[key] = ball_two[key] + 1
+            elsif key.to_i < 20
+              ball_three[key] = ball_three[key] + 1
+            elsif key.to_i < 27
+              ball_four[key] = ball_four[key] + 1
+            else
+              ball_five[key] = ball_five[key] + 1
+            end
           end
         end
       end
     end
-  end
 
-  # sorting out highest counts
-  positions = Array.new
-  positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
-  positions.each do |position|
-    g = position.values
-    g.sort!
-    position.delete_if {|k, v| v < g.last}
-  end
-
-  return positions
-
-end
-
-################################################################
-################################################################
-
-
-def cash4life
-
-  # get the source
-  doc = Nokogiri::HTML(URI.open('https://lottery.com/previous-results/pa/cash4life/2021/'))
-  results = []
-  doc.css('td').each do |data|
-    results.push(data.content)
-  end
-
-  latest_draws = Array.new #holds strings of numbers
-
-  #push numbers into array and only get enough so that all numbers are accounted for
-  counter = 3
-  100.times do
-    latest_draws.push(results[counter].split('-'))
-    counter = counter + 5
-    break if latest_draws.flatten.uniq.length == 60
-  end
-
-  #put all 30 numbers into an array as strings
-  all_numbers = Array.new
-  60.times do |t|
-    all_numbers.push((t+1).to_s)
-  end
-
-  #create hash with all numbers and set value to zero
-  candidates = Hash.new
-  all_numbers.each do |a|
-    candidates[a] = 0
-  end
-
-  #set all numbers that were drawn to 1
-  6.times do |draw|
-    latest_draws[draw].each do |pick|
-      candidates[pick] = 1
+    # sorting out highest counts
+    positions = Array.new
+    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
+    positions.each do |position|
+      g = position.values
+      g.sort!
+      position.delete_if {|k, v| v < g.last}
     end
-  end
-  
-  #remove all numbers that were drawn
-  candidates.delete_if {|k, v| v == 1}
-  
-  #initialize ranges
 
-  ball_five = Hash.new
-  all_numbers.pop(6).each do |b|
-    candidates.each do |key, value|
-      if key == b
-        ball_five[key] = 0
+    return positions
+
+  end
+
+  ################################################################
+  ################################################################
+
+
+  def cash4life
+
+    # get the source
+    doc = Nokogiri::HTML(URI.open('https://lottery.com/previous-results/pa/cash4life/2021/'))
+    results = []
+    doc.css('td').each do |data|
+      results.push(data.content)
+    end
+
+    latest_draws = Array.new #holds strings of numbers
+
+    #push numbers into array and only get enough so that all numbers are accounted for
+    counter = 3
+    100.times do
+      latest_draws.push(results[counter].split('-'))
+      counter = counter + 5
+      break if latest_draws.flatten.uniq.length == 60
+    end
+
+    #put all 30 numbers into an array as strings
+    all_numbers = Array.new
+    60.times do |t|
+      all_numbers.push((t+1).to_s)
+    end
+
+    #create hash with all numbers and set value to zero
+    candidates = Hash.new
+    all_numbers.each do |a|
+      candidates[a] = 0
+    end
+
+    #set all numbers that were drawn to 1
+    6.times do |draw|
+      latest_draws[draw].each do |pick|
+        candidates[pick] = 1
       end
     end
-  end
+    
+    #remove all numbers that were drawn
+    candidates.delete_if {|k, v| v == 1}
+    
+    #initialize ranges
 
-  ball_four = Hash.new
-  all_numbers.pop(16).each do |c|
-    candidates.each do |key, value|
-      if key == c
-        ball_four[key] = 0
+    ball_five = Hash.new
+    all_numbers.pop(6).each do |b|
+      candidates.each do |key, value|
+        if key == b
+          ball_five[key] = 0
+        end
       end
     end
-  end
 
-  ball_three = Hash.new
-  all_numbers.pop(16).each do |d|
-    candidates.each do |key, value|
-      if key == d
-        ball_three[key] = 0
+    ball_four = Hash.new
+    all_numbers.pop(16).each do |c|
+      candidates.each do |key, value|
+        if key == c
+          ball_four[key] = 0
+        end
       end
     end
-  end
 
-  ball_two = Hash.new
-  all_numbers.pop(16).each do |e|
-    candidates.each do |key, value|
-      if key == e
-        ball_two[key] = 0
+    ball_three = Hash.new
+    all_numbers.pop(16).each do |d|
+      candidates.each do |key, value|
+        if key == d
+          ball_three[key] = 0
+        end
       end
     end
-  end
 
-  ball_one = Hash.new
-  all_numbers.each do |f|
-    candidates.each do |key, value|
-      if key == f
-        ball_one[key] = 0
+    ball_two = Hash.new
+    all_numbers.pop(16).each do |e|
+      candidates.each do |key, value|
+        if key == e
+          ball_two[key] = 0
+        end
       end
     end
-  end
 
-  # get counts for each number drawn and fill in ranges
-  candidates.each do |key, value|
-    latest_draws.each do |draw|
-      draw.each do |j|
-        if key == j
-          if key.to_i < 6
-            ball_one[key] = ball_one[key] + 1
-          elsif key.to_i < 22
-            ball_two[key] = ball_two[key] + 1
-          elsif key.to_i < 38
-            ball_three[key] = ball_three[key] + 1
-          elsif key.to_i < 54
-            ball_four[key] = ball_four[key] + 1
-          else
-            ball_five[key] = ball_five[key] + 1
+    ball_one = Hash.new
+    all_numbers.each do |f|
+      candidates.each do |key, value|
+        if key == f
+          ball_one[key] = 0
+        end
+      end
+    end
+
+    # get counts for each number drawn and fill in ranges
+    candidates.each do |key, value|
+      latest_draws.each do |draw|
+        draw.each do |j|
+          if key == j
+            if key.to_i < 6
+              ball_one[key] = ball_one[key] + 1
+            elsif key.to_i < 22
+              ball_two[key] = ball_two[key] + 1
+            elsif key.to_i < 38
+              ball_three[key] = ball_three[key] + 1
+            elsif key.to_i < 54
+              ball_four[key] = ball_four[key] + 1
+            else
+              ball_five[key] = ball_five[key] + 1
+            end
           end
         end
       end
     end
-  end
 
-  # sorting out highest counts
-  positions = Array.new
-  positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
-  positions.each do |position|
-    g = position.values
-    g.sort!
-    position.delete_if {|k, v| v < g.last}
-  end
+    # sorting out highest counts
+    positions = Array.new
+    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
+    positions.each do |position|
+      g = position.values
+      g.sort!
+      position.delete_if {|k, v| v < g.last}
+    end
 
-  return positions
+    return positions
+
+  end
 
 end
