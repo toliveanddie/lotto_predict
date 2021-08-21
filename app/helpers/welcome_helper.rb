b7f9e036d0cb0aeaa45b605fa9a4781272afbd50
norm <toliveanddie76@gmail.com>
2021-08-21 15:31:09 -0400

    rewrote the counting methods for powerball and megamillions

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -2,7 +2,7 @@ module WelcomeHelper
 
   def powerball
     # get the source
-    doc = Nokogiri::HTML(URI.open('https://www.lotteryusa.com/pennsylvania/powerball/year'))
+    doc = Nokogiri::HTML(URI.open('https://www.lotteryusa.com/powerball/year'))
     results = []
     doc.css('li').each do |data|
       d = data.content.strip
@@ -10,108 +10,56 @@ module WelcomeHelper
         results.push(d)
       end
     end
-
-    latest_draws = Array.new #holds strings of numbers
+    latest_draws = Array.new # holds latest draws
+    draws = Array.new #holds all draww
+    splits = [9,17,17,17,9]
+    ball_one = Hash.new
+    ball_two = Hash.new
+    ball_three = Hash.new
+    ball_four = Hash.new
+    ball_five = Hash.new
+    positions = Array.new
+    
+    positions = Array.new
+    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
 
     #push numbers into array and only get enough so that all numbers are accounted for
     100.times do
-      latest_draws.push(results.shift(5))
-      break if latest_draws.flatten.uniq.length == 69
+      draws.push(results.shift(5))
+      results.shift(1)
+      break if draws.flatten.uniq.length >= 69
     end
   
-    #put all 49 numbers into an array as strings
-    all_numbers = Array.new
-    69.times do |t|
-      all_numbers.push((t+1).to_s)
-    end
-
-    #create hash with all numbers and set value to zero
-    candidates = Hash.new
-    all_numbers.each do |a|
-      candidates[a] = 0
-    end
-
-    #set all numbers that were drawn to 1
-    7.times do |draw|
-      latest_draws[draw].each do |pick|
-        candidates[pick] = 1
-      end
-    end
-  
-    #remove all numbers that were drawn
-    candidates.delete_if {|k, v| v == 1}
-
-    #initialize ranges
-    ball_five = Hash.new
-    all_numbers.pop(9).each do |b|
-      candidates.each do |key, value|
-        if key == b
-          ball_five[key] = 0
-        end
+    c=1
+    5.times do |i|
+      splits[i].times do
+        positions[i][c.to_s] = 0
+        c+=1
       end
     end
 
-    ball_four = Hash.new
-    all_numbers.pop(17).each do |c|
-      candidates.each do |key, value|
-        if key == c
-          ball_four[key] = 0
-        end
-      end
-    end
-
-    ball_three = Hash.new
-    all_numbers.pop(17).each do |d|
-      candidates.each do |key, value|
-        if key == d
-          ball_three[key] = 0
-        end
-      end
-    end
-
-    ball_two = Hash.new
-    all_numbers.pop(17).each do |e|
-      candidates.each do |key, value|
-        if key == e
-          ball_two[key] = 0
-        end
-      end
-    end
-
-    ball_one = Hash.new
-    all_numbers.each do |f|
-      candidates.each do |key, value|
-        if key == f
-          ball_one[key] = 0
-        end
-      end
-    end
-
-
-    # get counts for each number drawn and fill in ranges
-    candidates.each do |key, value|
-      latest_draws.each do |draw|
-        draw.each do |g|
-          if key == g
-            if key.to_i < 10
-              ball_one[key] = ball_one[key] + 1
-            elsif key.to_i < 27
-              ball_two[key] = ball_two[key] + 1
-            elsif key.to_i < 44
-              ball_three[key] = ball_three[key] + 1
-            elsif key.to_i < 61
-              ball_four[key] = ball_four[key] + 1
-            else
-              ball_five[key] = ball_five[key] + 1
+    draws.each do |draw|
+      draw.each do |pick|
+        positions.each do |ball|
+          ball.each do |key,value|
+            if key == pick
+              ball[pick] = ball[pick] + 1
             end
           end
         end
       end
+    end    
+
+    latest_draws = draws.shift(7)
+    latest_draws.flatten!.uniq!.sort!
+
+    latest_draws.each do |pick|
+      positions.each do |ball|
+        ball.delete_if {|k,v| k == pick}
+      end
     end
 
     # sorting out highest counts
-    positions = Array.new
-    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
     positions.each do |position|
       g = position.values
       g.sort!
@@ -122,7 +70,7 @@ module WelcomeHelper
 
     total_ball = []
     doc.css('span').each do |span|
-      s = span.content
+      s = span.content.strip
       if s.to_i != 0
         total_ball.push(s)
       end
@@ -131,7 +79,7 @@ module WelcomeHelper
     ball = []
     100.times do |element|
       ball.push(total_ball[element])
-      break if ball.flatten.uniq.length == 25
+      #break if ball.flatten.uniq.length >= 26
     end
     all_balls = ball.flatten.uniq
 
@@ -161,7 +109,7 @@ module WelcomeHelper
 
   def megaball
     # get the source
-    doc = Nokogiri::HTML(URI.open('https://www.lotteryusa.com/pennsylvania/mega-millions/year'))
+    doc = Nokogiri::HTML(URI.open('https://www.lotteryusa.com/mega-millions/year'))
     results = []
     doc.css('li').each do |data|
       d = data.content.strip
@@ -169,108 +117,56 @@ module WelcomeHelper
         results.push(d)
       end
     end
-
-    latest_draws = Array.new #holds strings of numbers
+    latest_draws = Array.new # holds latest draws
+    draws = Array.new #holds all draww
+    splits = [9,17,18,17,9]
+    ball_one = Hash.new
+    ball_two = Hash.new
+    ball_three = Hash.new
+    ball_four = Hash.new
+    ball_five = Hash.new
+    positions = Array.new
+    
+    positions = Array.new
+    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
 
     #push numbers into array and only get enough so that all numbers are accounted for
     100.times do
-      latest_draws.push(results.shift(5))
-      break if latest_draws.flatten.uniq.length == 70
+      draws.push(results.shift(5))
+      results.shift(1)
+      break if draws.flatten.uniq.length >= 70
     end
   
-    #put all 49 numbers into an array as strings
-    all_numbers = Array.new
-    70.times do |t|
-      all_numbers.push((t+1).to_s)
-    end
-
-    #create hash with all numbers and set value to zero
-    candidates = Hash.new
-    all_numbers.each do |a|
-      candidates[a] = 0
-    end
-
-    #set all numbers that were drawn to 1
-    7.times do |draw|
-      latest_draws[draw].each do |pick|
-        candidates[pick] = 1
-      end
-    end
-  
-    #remove all numbers that were drawn
-    candidates.delete_if {|k, v| v == 1}
-
-    #initialize ranges
-    ball_five = Hash.new
-    all_numbers.pop(9).each do |b|
-      candidates.each do |key, value|
-        if key == b
-          ball_five[key] = 0
-        end
+    c=1
+    5.times do |i|
+      splits[i].times do
+        positions[i][c.to_s] = 0
+        c+=1
       end
     end
 
-    ball_four = Hash.new
-    all_numbers.pop(17).each do |c|
-      candidates.each do |key, value|
-        if key == c
-          ball_four[key] = 0
-        end
-      end
-    end
-
-    ball_three = Hash.new
-    all_numbers.pop(18).each do |d|
-      candidates.each do |key, value|
-        if key == d
-          ball_three[key] = 0
-        end
-      end
-    end
-
-    ball_two = Hash.new
-    all_numbers.pop(17).each do |e|
-      candidates.each do |key, value|
-        if key == e
-          ball_two[key] = 0
-        end
-      end
-    end
-
-    ball_one = Hash.new
-    all_numbers.each do |f|
-      candidates.each do |key, value|
-        if key == f
-          ball_one[key] = 0
-        end
-      end
-    end
-
-
-    # get counts for each number drawn and fill in ranges
-    candidates.each do |key, value|
-      latest_draws.each do |draw|
-        draw.each do |g|
-          if key == g
-            if key.to_i < 10
-              ball_one[key] = ball_one[key] + 1
-            elsif key.to_i < 27
-              ball_two[key] = ball_two[key] + 1
-            elsif key.to_i < 45
-              ball_three[key] = ball_three[key] + 1
-            elsif key.to_i < 62
-              ball_four[key] = ball_four[key] + 1
-            else
-              ball_five[key] = ball_five[key] + 1
+    draws.each do |draw|
+      draw.each do |pick|
+        positions.each do |ball|
+          ball.each do |key,value|
+            if key == pick
+              ball[pick] = ball[pick] + 1
             end
           end
         end
       end
     end
 
+    latest_draws = draws.shift(7)
+    latest_draws.flatten!.uniq!.sort!
+
+    latest_draws.each do |pick|
+      positions.each do |ball|
+        ball.delete_if {|k,v| k == pick}
+      end
+    end
+
     # sorting out highest counts
-    positions = Array.new
-    positions = [ball_one, ball_two, ball_three, ball_four, ball_five]
     positions.each do |position|
       g = position.values
       g.sort!
@@ -290,7 +186,7 @@ module WelcomeHelper
     ball = []
     100.times do |element|
       ball.push(total_ball[element])
-      break if ball.flatten.uniq.length == 25
+      #break if ball.flatten.uniq.length >= 25
     end
     all_balls = ball.flatten.uniq
     
@@ -332,9 +228,9 @@ module WelcomeHelper
     latest_draws = Array.new #holds strings of numbers
 
     #push numbers into array and only get enough so that all numbers are accounted for
-    360.times do
+    100.times do
       latest_draws.push(results.shift(6))
-      break if latest_draws.flatten.uniq.length == 49
+      break if latest_draws.flatten.uniq.length >= 49
     end
 
     latest_draws.shift
@@ -472,7 +368,7 @@ module WelcomeHelper
 
     #push numbers into array and only get enough so that all numbers are accounted for
     counter = 3
-    360.times do
+    100.times do
       latest_draws.push(results.shift(5))
       break if latest_draws.flatten.uniq.length >= 43
     end
@@ -599,9 +495,9 @@ module WelcomeHelper
 
     #push numbers into array and only get enough so that all numbers are accounted for
     
-    360.times do
+    100.times do
       latest_draws.push(results.shift(5))
-      break if latest_draws.flatten.uniq.length == 30
+      break if latest_draws.flatten.uniq.length >= 30
     end
 
     #put all 30 numbers into an array as strings
@@ -726,9 +622,9 @@ module WelcomeHelper
     latest_draws = Array.new #holds strings of numbers
 
     #push numbers into array and only get enough so that all numbers are accounted for
-    360.times do
+    100.times do
       latest_draws.push(results.shift(5))
-      break if latest_draws.flatten.uniq.length == 60
+      break if latest_draws.flatten.uniq.length >= 60
     end
   
     #put all 49 numbers into an array as strings
